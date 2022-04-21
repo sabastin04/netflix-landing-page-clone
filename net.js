9b082f35ca0c0e67d7322b219e1f409c251dca93
sabastin04 <104013492+sabastin04@users.noreply.github.com>
2022-04-21 15:38:56 +0100

    netflix html and css

---
+++ net.js
@@ -0,0 +1,7 @@
+list_items=["Go back","Remember password"]
+const flest=document.querySelector(".flest")
+flest.innerText=list_items[0]
+
+
+const fles=document.querySelector(".fles")
+fles.innerText=list_items[1]
